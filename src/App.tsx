d68be4c99c41b971e5f38d1f7bee5f2a250a7ea8
Antonio Contreras <bahlone@gmail.com>
2025-07-26 18:20:13 -0700

    Adding Vite (React + TypeScript) project with Material UI and TanStack Query (#1)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import HelloWorld from "./components/HelloWorld";
+// import "./App.css";
+
+const App: React.FC = () => {
+  // JSX
+  return (
+    <>
+      <h1>NASA image search</h1>
+      <HelloWorld />
+    </>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//       retry: false,
+//     },
+//   },
+// });
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
